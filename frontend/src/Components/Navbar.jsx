92d934ba3dd9c8c238c50432d4894f63fbe68c5c
amaan7355 <alamamaan334@gmail.com>
2023-09-22 20:01:53 +0530

    ..

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom';
+import UseAppContext from '../AppContext';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+
+    const { loggedIn, logout } = UseAppContext();
+
+    const [currentUser, setCurrentUser] = useState(
+        JSON.parse(sessionStorage.getItem('user'))
+    );
+
+    const displayUserOption = () => {
+        if (loggedIn) {
+            return ( 
+            <>
+            <li className='nav-item'><button className='btn btn-danger' onClick={logout}>Logout</button></li>
+            <li>
+                <img height={50} className='rounded-circle' src={"http://localhost:5000/" +currentUser.avatar} alt="avatar" />
+            </li>
+            </>
+            )
+        }
+        else{
+            return (
+            <>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/login">
+                        Login
+                    </NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="/signup">
+                        Signup
+                    </NavLink>
+                </li>
+            </>
+            )
+        }
+    }
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-territory">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <h2>React</h2>
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav mx-3 me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/">
+                                Home
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <ul className='navbar-nav ms-auto mb-2 mb-lg-0'>
+                        {displayUserOption()}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+    )
 }
 
 export default Navbar;
--- frontend/src/Components/NotFound.jsx
+++ frontend/src/Components/NotFound.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='vh-100'>
+        <h1 className='text-center display-1 fw-bold'>404</h1>
+        <h1 className='text-center display-2 fw-bold'>Page Not Found</h1>
+
+    </div>
   )
 }
 
