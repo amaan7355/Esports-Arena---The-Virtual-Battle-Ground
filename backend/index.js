38e89c4151b45c51bef9f669e81aa1eb29cf29b2
amaan7355 <alamamaan334@gmail.com>
2023-12-14 20:09:59 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = 5000;
 // middlewares
 app.use(express.json());
 app.use(cors({
-    origin: [ 'http://localhost:3000', 'https://esportarena.onrender.com/' ]
+    origin: [ 'http://localhost:3000', 'https://esportarena.onrender.com' ]
 }));
 
 app.use('/user', userRouter);
